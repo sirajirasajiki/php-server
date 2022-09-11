c7ed9ad25e5919b6cc7156df215abde089e16ec7
sirajirasajiki <takagikousuke02@gmail.com>
2022-09-11 20:58:54 +0900

    実用的な例の実行。

--- index.php
+++ index.php
@@ -5,5 +5,16 @@
 </head>
 <body>
     <?php echo '<p>Hello World</p>'; ?> 
+    <?php
+    echo $_SERVER['HTTP_USER_AGENT'];
+    // 分岐
+    if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false) {
+        echo 'あなたはInternet Explorerを使用しています<br />';
+      }
+    else{
+        echo 'あなたはInternet Explorerを使用していません<br />';
+    }
+    
+    ?>
 </body>
 </html>
